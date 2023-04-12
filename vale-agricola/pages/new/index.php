3a4ea6d61b0ae0eb64b6d8e5c29dd8bc7d646e9b
marceloost <104876213+marceloost@users.noreply.github.com>
2023-04-12 14:03:26 -0300

    up

--- vale-agricola/pages/new/index.php
+++ vale-agricola/pages/new/index.php
@@ -25,7 +25,6 @@ if (isset($_POST["button"])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="../../assets/images/olifx_logo.png" type="image/png">
   <title>Vale Agrícola | Criar conta</title>
   <script src="scripts.js"></script>
   <link rel="stylesheet" href="new.css">
