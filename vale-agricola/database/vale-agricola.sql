a83ec98702f1b3cb37b745bd55e1f4774bf98c30
marceloost <104876213+marceloost@users.noreply.github.com>
2023-04-27 15:28:01 -0300

    up

--- vale-agricola/database/vale-agricola.sql
+++ vale-agricola/database/vale-agricola.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 11-Abr-2023 às 21:49
+-- Tempo de geração: 27-Abr-2023 às 19:40
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `documento`
+--
+
+CREATE TABLE `documento` (
+  `idDocumento` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `validade` date DEFAULT NULL,
+  `pdf` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `documento`
+--
+
+INSERT INTO `documento` (`idDocumento`, `nome`, `validade`, `pdf`) VALUES
+(30, 'teste', '2025-05-17', 0x2e2e2f2e2e2f646f63756d656e746f732f41727469676f2031352e706466),
+(31, 'teste2', '2028-02-28', 0x2e2e2f2e2e2f646f63756d656e746f732f41636164c3aa6d69636f2e706466),
+(32, 'teste 3', '2028-03-25', 0x2e2e2f2e2e2f646f63756d656e746f732f4f20536f6d20646f2052756769646f206461204f6ec3a7612e706466),
+(33, 'Agro Ost II', '2025-10-17', 0x2e2e2f2e2e2f646f63756d656e746f732f617465737461646f2e706466);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `empresa`
 --
 
@@ -32,7 +55,7 @@ CREATE TABLE `empresa` (
   `nome` varchar(255) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `cnpj` varchar(14) DEFAULT NULL
+  `cnpj` varchar(18) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -40,11 +63,17 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`idEmpresa`, `nome`, `senha`, `email`, `cnpj`) VALUES
-(1, 'Marcelo Agroferagem', '$2y$10$74d1anjeYSF2lCWWZ6vhxOPRAUrXdQXHMmK7VPHKil1gHrkv73jkK', 'marcelo@gmail.com', '12312312312');
+(101, 'Agro Ost', '$2y$10$DdiZxK5RVfqZ42vCESDnSuYfBBp/JZmRu/Z3VMkSsb/O/L5SoUkiW', 'marcelo7@gmail.com', '25.615.161/5615-61');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `documento`
+--
+ALTER TABLE `documento`
+  ADD PRIMARY KEY (`idDocumento`);
 
 --
 -- Índices para tabela `empresa`
@@ -58,10 +87,16 @@ ALTER TABLE `empresa`
 --
 
 --
+-- AUTO_INCREMENT de tabela `documento`
+--
+ALTER TABLE `documento`
+  MODIFY `idDocumento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- AUTO_INCREMENT de tabela `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `idEmpresa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idEmpresa` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
