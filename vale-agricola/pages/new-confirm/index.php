382de72fa57b70d82be9501568bab9991df49c30
marceloost <104876213+marceloost@users.noreply.github.com>
2023-05-08 16:57:04 -0300

    up

--- vale-agricola/pages/new-confirm/index.php
+++ vale-agricola/pages/new-confirm/index.php
@@ -11,10 +11,26 @@ require_once "../../settings/config.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vale Agrícola | Confirmação de Cadastro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Cadastro concluído com Sucesso!</h1>
-    <p>Clique no botão "Login" para voltar à página de acesso, realizar o login e conseguir acessar a página do programa.</p>
-    <a href="../login">Login</a>
+    <header class="header">
+        <div class="logo">
+            <img src="../../settings/imagens/logo-alto-feliz.png" alt="logoaf">
+        </div>
+        <div class="icone">
+            <img src="../../settings/imagens/icone-contraste.png" alt="iconedl">
+        </div>
+    </header>
+
+    <div class="login-util">
+        <div class="login">
+            <h1 class="titulo">Cadastro concluído com Sucesso!</h1>
+
+            <p class="conteudo">Clique no botão abaixo para voltar à página de acesso, realizar o login e acessar a página do programa.</p>
+
+            <a href="../login" class="botao">Login</a>
+        </div>
+    </div>
 </body>
 </html>
