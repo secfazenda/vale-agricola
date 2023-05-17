316b8efe13f5eef6becc2a0550a143532063d83c
marceloost <104876213+marceloost@users.noreply.github.com>
2023-05-17 16:58:27 -0300

    up

--- vale-agricola/pages/edit-document/index.php
+++ vale-agricola/pages/edit-document/index.php
@@ -11,9 +11,20 @@ if (isset($_GET["idDocumento"])) {
 
     // Verifique se o documento foi encontrado
     if ($documento) {
-        echo "<h1>{$documento->getNome()}</h1>";
-        echo "<p>Validade: {$documento->getValidade()->format("d/m/Y")}</p>";
+        // echo "<h1>{$documento->getNome()}</h1>";
+        // echo "<p>Validade: {$documento->getValidade()->format("d/m/Y")}</p>";
         // Resto do código para exibir as informações do documento
+
+        if (isset($_POST["idDocumento"])) {
+            $idDocumento = $_POST["idDocumento"];
+            $nome = $_POST["nome"];
+            $validade = $_POST["validade"];
+
+            // Resto do código de edição...
+            $documento->setNome($nome);
+            $documento->setValidade($validade);
+            $documento->save(); // Salvar as alterações no banco de dados
+        }
     } else {
         echo "Documento não encontrado.";
     }
@@ -30,25 +41,36 @@ if (isset($_GET["idDocumento"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Vale Agrícola | Documento</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <header class="header">
+        <div class="logo">
+            <img src="../../settings/imagens/logo-alto-feliz.png" alt="logoaf">
+        </div>
+        <div class="icone">
+            <img src="../../settings/imagens/icone-contraste.png" alt="iconedl">
+        </div>
+    </header>
+
     <div class="edit-document-util">
         <div class="edit-document">
             <h1 class="titulo">Editar Documento</h1>
-            <!-- Formulário de edição do documento -->
+
             <form action="index.php" method="post" enctype="multipart/form-data">
-                <!-- Campos do formulário -->
-                <label for="nome">Nome:</label>
+                <input type="hidden" name="idDocumento" value="<?php echo $idDocumento; ?>">
+                <label for="nome">Nome</label>
                 <input type="text" id="nome" name="nome" value="<?php echo $documento->getNome(); ?>"><br>
-                <label for="validade">Validade:</label>
+                <label for="validade">Validade</label>
                 <input type="text" id="validade" name="validade" value="<?php echo $documento->getValidade()->format("d/m/Y"); ?>"><br>
-                <!-- ... outros campos do formulário ... -->
 
                 <input type="submit" value="Editar" class="botao" name="button">
             </form>
-
-            <a class="excluir" href="../delete-document?idDocumento=<?php echo $documento->getIdDocumento(); ?>" onclick="return confirmarExclusao()">Excluir Documento</a>
+            
+            <a class="botao-excluir" href="../delete-document?idDocumento=<?php echo $documento->getIdDocumento(); ?>" onclick="return confirmarExclusao()">Excluir Documento</a>
             <a href="../home" class="botao-voltar"><img src="../../settings/imagens/botao-voltar.png" alt=""></a>
+            
         </div>
     </div>
 </body>
