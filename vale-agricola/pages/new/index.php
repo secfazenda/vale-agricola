0bcfeb8205666368741a4046e34c8ca890810d3d
marceloost <104876213+marceloost@users.noreply.github.com>
2023-05-30 14:16:10 -0300

    up

--- vale-agricola/pages/new/index.php
+++ vale-agricola/pages/new/index.php
@@ -1,10 +1,7 @@
 <?php
-
 require_once "../../settings/config.php";
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
 
-if (isset($_POST["button"])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $empresas = Empresa::findall();
     
     $nome = trim($_POST["nome"]);
@@ -14,34 +11,41 @@ if (isset($_POST["button"])) {
     $erro = false;
 
     foreach ($empresas as $empresa) {
-        if($empresa->getNome() == $nome){
-            echo "<div class='div-mensagem-erro'><h3 class='mensagem-erro'>O nome já está sendo utilizado.</h3></div>";
+        if (strtolower($empresa->getNome()) === strtolower($nome)) {
             $erro = true;
+            $mensagemErro = "O nome já está sendo utilizado.";
             break;
-        } else if($empresa->getEmail() == $email){
-            echo "<div class='div-mensagem-erro'><h3 class='mensagem-erro'>E-mail já está sendo utilizado.</h3></div>";
+        } else if (strtolower($empresa->getEmail()) === strtolower($email)) {
             $erro = true;
+            $mensagemErro = "E-mail já está sendo utilizado.";
             break;
-        } else if($empresa->getCnpj() == $cnpj){
-            echo "<div class='div-mensagem-erro'><h3 class='mensagem-erro'>Cnpj já está sendo utilizado.</h3></div>";
+        } else if ($empresa->getCnpj() === $cnpj) {
             $erro = true;
+            $mensagemErro = "CNPJ já está sendo utilizado.";
             break;
-            }
         }
+      if (strlen($cnpj) < 14) {
+        $erro = true;
+        $mensagemErro = "CNPJ inválido.";
+    }
 
-        if (!$erro){
-            $empresa = new Empresa();
-            $empresa->constructorCreate(
-                $nome,
-                trim($_POST["senha"]),
-                $email,
-                $cnpj
-          );
-          $empresa->save();
-          header("location: ../new-confirm/");
-        }
+    }
+
+    if ($erro) {
+        echo "<div class='div-mensagem-erro'><h3 class='mensagem-erro'>$mensagemErro</h3></div>";
+    } else {
+        $empresa = new Empresa();
+        $empresa->constructorCreate(
+            $nome,
+            trim($_POST["senha"]),
+            $email,
+            $cnpj
+        );
+        $empresa->save();
+        header("location: ../new-confirm/");
+        exit;
+    }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -68,9 +72,9 @@ if (isset($_POST["button"])) {
     <div class="create-account-util">
         <div class="create-account">
             <h1 class="titulo">Criar Conta</h1>
-            <form action="index.php" method="post" enctype="multipart/form-data">
+            <form action="index.php" method="post" enctype="multipart/form-data" id="create-account-form">
 
-                <label for="cnpj">Cnpj</label>
+                <label for="cnpj">CNPJ</label>
                 <input type="string" name="cnpj" id="cnpj" minlength="14" required>
 
                 <label for="fullname">Nome</label>
@@ -85,7 +89,6 @@ if (isset($_POST["button"])) {
                   <button type="button" class="olho-senha" name="olho-senha-nova-senha"><i class="fa fa-eye"></i></button>
                 </div>
 
-                
                 <label for="password">Confirmar Senha</label>
                 <div>
                   <input type="password" name="confirmar-senha" id="confirmar-senha" minlength="6" required>
@@ -98,104 +101,93 @@ if (isset($_POST["button"])) {
             </form>
         </div>
     </div>
+
+    <!-- Incluindo o jQuery e o jQuery Mask Plugin -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+    <script>
+      var botoesOlhoSenha = document.getElementsByClassName("olho-senha");
+
+      Array.from(botoesOlhoSenha).forEach(function(botaoOlhoSenha) {
+          botaoOlhoSenha.addEventListener("click", function() {
+              var senhaInput = this.previousElementSibling;
+              
+              if (senhaInput.type === "password") {
+                  senhaInput.type = "text";
+                  this.innerHTML = '<i class="fa fa-eye-slash"></i>';
+              } else {
+                  senhaInput.type = "password";
+                  this.innerHTML = '<i class="fa fa-eye"></i>';
+              }
+          });
+      });
+
+      document.getElementById("create-account-form").addEventListener("submit", function(event) {
+          var cnpj = document.getElementById("cnpj").value;
+          var nome = document.getElementById("nome").value;
+          var email = document.getElementById("email").value;
+          var senha = document.getElementById("senha").value;
+          var confirmarSenha = document.getElementById("confirmar-senha").value;
+
+          if (cnpj.trim() === '' || nome.trim() === '' || email.trim() === '' || senha.trim() === '' || confirmarSenha.trim() === '' || !isValidEmail(email)) {
+              alert("Por favor, preencha os campos corretamente.");
+              event.preventDefault();
+          } else if (senha !== confirmarSenha) {
+              alert("As senhas não coincidem.");
+              event.preventDefault();
+          } else {
+              alert("Sua conta foi criada com sucesso.");
+              // confirmarCriacao();
+          }
+      });
+
+      $(document).ready(function(){
+        $('#cnpj').mask('00.000.000/0000-00', {
+          onComplete: function(cnpj) {
+            var cnpjLength = cnpj.replace(/\D/g, '').length;
+            if (cnpjLength < 14) {
+              $('#cnpj').val('');
+            }
+          }
+        });
+      });
+
+      $(document).ready(function() {
+        $('#nome').mask('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', {
+          translation: {
+          'A': { pattern: /[A-Za-zÀ-ú\s]/ }
+          }
+        });
+      });
+
+      $(document).ready(function(){
+        $('#email').mask("A", {
+          translation: {
+            "A": { pattern: /[\w@\-.+]/, recursive: true }
+          }
+        }).on("blur", function(){
+          let email = $(this).val();
+          if(!isValidEmail(email)){
+            $(this).val("");
+          }
+        });
+      });
+
+      function isValidEmail(email) {
+        let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+      }
+
+      $(document).ready(function(){
+        $('#confirmar-senha').on('input', function() {
+          if ($(this).val() != $('#senha').val()) {
+            this.setCustomValidity("As senhas não coincidem");
+          } else {
+            this.setCustomValidity('');
+          }
+        });
+      });
+    </script>
 </body>
 </html>
-
-<!-- Incluindo o jQuery e o jQuery Mask Plugin -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-
-<script>
-  document.querySelector('.botao').addEventListener('click', function() {
-    var cnpj = document.getElementById("cnpj").value;
-    var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-    var confirmarSenha = document.getElementById("confirmar-senha").value;
-
-    if (cnpj.trim() === '' || nome.trim() === '' || email.trim() === '' || senha.trim() === '' || confirmarSenha.trim() === '' || !validarEmail(email)) {
-        alert("Por favor, preencha os campos corretamente.");
-        event.preventDefault();
-    } else if (senha !== confirmarSenha) {
-        alert("As senhas não coincidem.");
-        event.preventDefault();
-    } else {
-        alert("Sua conta foi criada com sucesso.");
-        // confirmarCriacao();
-    }
-});
-</script>
-
-<!-- Aplicando a máscara de CNPJ -->
-<script>
-$(document).ready(function(){
-  $('#cnpj').mask('00.000.000/0000-00', {
-    onComplete: function(cnpj) {
-      var cnpjLength = cnpj.replace(/\D/g, '').length;
-      if (cnpjLength < 14) {
-        $('#cnpj').val('');
-      }
-    }
-  });
-});
-</script>
-
-<!-- Aplicando a máscara de Nome -->
-<script>
-  $(document).ready(function() {
-    $('#nome').mask('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', {
-      translation: {
-      'A': { pattern: /[A-Za-zÀ-ú\s]/ }
-      }
-    });
-  });
-</script>
-
-<!-- Aplicando a máscara de e-mail -->
-<script>
-$(document).ready(function(){
-  $('#email').mask("A", {
-    translation: {
-      "A": { pattern: /[\w@\-.+]/, recursive: true }
-    }
-  }).on("blur", function(){
-    let email = $(this).val();
-    if(!isValidEmail(email)){
-      $(this).val("");
-    }
-  });
-});
-
-function isValidEmail(email) {
-  let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
-$(document).ready(function(){
-  $('#confirmar-senha').on('input', function() {
-    if ($(this).val() != $('#senha').val()) {
-      this.setCustomValidity("As senhas não coincidem");
-    } else {
-      this.setCustomValidity('');
-    }
-  });
-});
-
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
-<script>
-var botoesOlhoSenha = document.getElementsByClassName("olho-senha");
-
-Array.from(botoesOlhoSenha).forEach(function(botaoOlhoSenha) {
-    botaoOlhoSenha.addEventListener("click", function() {
-        var senhaInput = this.previousElementSibling;
-        
-        if (senhaInput.type === "password") {
-            senhaInput.type = "text";
-            this.innerHTML = '<i class="fa fa-eye-slash"></i>';
-        } else {
-            senhaInput.type = "password";
-            this.innerHTML = '<i class="fa fa-eye"></i>';
-        }
-    });
-});
-</script>
