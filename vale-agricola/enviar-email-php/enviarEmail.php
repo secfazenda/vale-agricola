d5230298269c7c5f2de74caa5fa5675a8ee9e16c
marceloost <104876213+marceloost@users.noreply.github.com>
2023-06-05 14:56:41 -0300

    up

--- vale-agricola/enviar-email-php/enviarEmail.php
+++ vale-agricola/enviar-email-php/enviarEmail.php
@@ -8,23 +8,33 @@ use PHPMailer\PHPMailer\PHPMailer;
 $EMAIL_ADDRESS = 'marcelo.ost7@gmail.com';
 $EMAIL_PASSWORD = 'jqolwaclkzozhrum';
 
-$mail = new PHPMailer();
-$mail->isSMTP();
-$mail->SMTPDebug = 0;
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;
-$mail->Username = $EMAIL_ADDRESS;
-$mail->Password = $EMAIL_PASSWORD;
-$mail->setFrom($EMAIL_ADDRESS);
-$mail->addAddress($EMAIL_ADDRESS);
-$mail->Subject = 'Vencimento de Documentos';
-$mail->Body = 'Olá, esse é um aviso para você dar uma olhada na validade dos documentos cadastrados no Vale Agricola, pois algum está prestes a expirar.';
+$intervalo = 86400;
 
-if (!$mail->send()) {
-    echo 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
-} else {
-    echo 'E-mail enviado com sucesso!';
+while (true) {
+    enviarEmail($EMAIL_ADDRESS, $EMAIL_PASSWORD);
+
+    sleep($intervalo);
+}
+
+function enviarEmail($email, $senha){
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAuth = true;
+    $mail->Username = $email;
+    $mail->Password = $senha;
+    $mail->setFrom($email);
+    $mail->addAddress($email);
+    $mail->Subject = 'Vencimento de Documentos';
+    $mail->Body = 'Olá, esse é um aviso para você dar uma olhada na validade dos documentos cadastrados no Vale Agricola, pois algum está prestes a expirar.';
+
+    if (!$mail->send()) {
+        echo 'Erro ao enviar o e-mail: ' . $mail->ErrorInfo;
+    } else {
+        echo 'E-mail enviado com sucesso!';
+    }
 }
 ?>
