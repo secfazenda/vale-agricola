4e9d9f513cc9a2b58bd4bd273675f7151e141b89
marceloost <104876213+marceloost@users.noreply.github.com>
2023-06-16 16:54:43 -0300

    up

--- vale_agricola/pages/delete_parte_prefeitura/index.php
+++ vale_agricola/pages/delete_parte_prefeitura/index.php
@@ -8,25 +8,20 @@ if (!isset($_SESSION["idEmpresa"])) {
 }
 
 if (!isset($_GET["idEmpresa"])) {
-    // Redirecionar para a página anterior ou exibir uma mensagem de erro
     header("Location: {$_SERVER['HTTP_REFERER']}");
     exit;
 }
 
 $idEmpresa = $_GET["idEmpresa"];
 
-// Verificar se o ID da empresa não é igual ao ID da prefeitura (1)
 if ($idEmpresa == 1) {
-    // Redirecionar para a página anterior ou exibir uma mensagem de erro
     header("Location: {$_SERVER['HTTP_REFERER']}");
     exit;
 }
 
-// Excluir a empresa com o ID correspondente
 $empresa = Empresa::find($idEmpresa);
 $empresa->delete();
 
-// Redirecionar para a página anterior ou exibir uma mensagem de sucesso
-header("Location: {$_SERVER['HTTP_REFERER']}");
+header("Location: ../home_prefeitura");
 exit;
 ?>
