1a4e70636fea103ecfcd722988ada2f4264bb586
marceloost <104876213+marceloost@users.noreply.github.com>
2023-07-11 14:23:10 -0300

    up

--- vale_agricola/database/vale-agricola.sql
+++ vale_agricola/database/vale-agricola.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04/07/2023 às 19:24
+-- Tempo de geração: 11-Jul-2023 às 19:22
 -- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `documento`
+-- Estrutura da tabela `documento`
 --
 
 CREATE TABLE `documento` (
@@ -36,7 +36,7 @@ CREATE TABLE `documento` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `documento`
+-- Extraindo dados da tabela `documento`
 --
 
 INSERT INTO `documento` (`idDocumento`, `nome`, `validade`, `pdf`, `idEmpresa`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `documento` (`idDocumento`, `nome`, `validade`, `pdf`, `idEmpresa`) 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `empresa`
+-- Estrutura da tabela `empresa`
 --
 
 CREATE TABLE `empresa` (
@@ -53,36 +53,39 @@ CREATE TABLE `empresa` (
   `nome` varchar(255) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `cnpj` varchar(18) DEFAULT NULL
+  `cnpj` varchar(18) DEFAULT NULL,
+  `habilitada` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `empresa`
+-- Extraindo dados da tabela `empresa`
 --
 
-INSERT INTO `empresa` (`idEmpresa`, `nome`, `senha`, `email`, `cnpj`) VALUES
-(1, 'Prefeitura', '$2y$10$gcVOgSxcOdmk/BXEdLtar.4Zgfzc/3a/bWH.zTHq6yQgFBWmLeTAy', 'marcelo.ost7@gmail.com', '92.123.926/0001-92'),
-(119, 'Marcelo Agroferagem', '$2y$10$Hp8.jq9hTpnU4irSQingGuFWNyAfkwkSqKISjT2CjvVP3YCU3r1/O', 'marcelo@gmail.com', '20.548.484/1548-48');
+INSERT INTO `empresa` (`idEmpresa`, `nome`, `senha`, `email`, `cnpj`, `habilitada`) VALUES
+(1, 'Prefeitura', '$2y$10$gcVOgSxcOdmk/BXEdLtar.4Zgfzc/3a/bWH.zTHq6yQgFBWmLeTAy', 'marcelo.ost7@gmail.com', '92.123.926/0001-92', 0),
+(119, 'Marcelo Agroferagem', '$2y$10$Hp8.jq9hTpnU4irSQingGuFWNyAfkwkSqKISjT2CjvVP3YCU3r1/O', 'marcelo@gmail.com', '20.548.484/1548-48', 0),
+(120, 'Agro Ost', '$2y$10$j4MimnucpTAmEZ1NVJpxN.WZK438VgNf4mjQAg8ZskI0F2zgSNVTG', 'agroindrustria@gmail.com', '02.154.789/5422-36', 0),
+(121, 'Agroferagem Marcelo', '$2y$10$Ivg1tIk506Qgsj3DQnAji.yzZxk9Kbj/Qqg9eQC65UdejaLlAkTjG', 'marceloost17@gmail.com', '06.250.652/0520-50', 0);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `documento`
+-- Índices para tabela `documento`
 --
 ALTER TABLE `documento`
   ADD PRIMARY KEY (`idDocumento`),
   ADD KEY `fk_empresa_documento` (`idEmpresa`);
 
 --
--- Índices de tabela `empresa`
+-- Índices para tabela `empresa`
 --
 ALTER TABLE `empresa`
   ADD PRIMARY KEY (`idEmpresa`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -95,14 +98,14 @@ ALTER TABLE `documento`
 -- AUTO_INCREMENT de tabela `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `documento`
+-- Limitadores para a tabela `documento`
 --
 ALTER TABLE `documento`
   ADD CONSTRAINT `fk_empresa_documento` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`);
